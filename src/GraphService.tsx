46e4944075f27fd559ea6c5e0b5779ed0edb8a66
speiser <sebastian.speiser@hft-stuttgart.de>
2021-08-09 12:33:53 +0200

    .

--- src/GraphService.tsx
+++ src/GraphService.tsx
@@ -1,17 +1,11 @@
 import { OnenotePage } from 'microsoft-graph';
 import { PageCollection, PageIterator } from '@microsoft/microsoft-graph-client';
 
-import { Writable } from 'stream';
-
 import { Writer } from '@transcend-io/conflux';
 import streamSaver from "streamsaver";
 import * as ponyfill from 'web-streams-polyfill/ponyfill';
 
 import TurndownService from 'turndown';
-
-import SparkMD5 from 'spark-md5';
-
-
 
 var graph = require('@microsoft/microsoft-graph-client');
 
@@ -162,88 +156,6 @@ export async function getUserDetails(accessToken: string) {
 
     return user;
 }
-
-// class MD5Stream extends Writable {
-//     spark: SparkMD5;
-
-//     constructor() {
-//         super({write: this._write}, new CountQueuingStrategy({ highWaterMark: 1 });
-//         this.spark = new SparkMD5();
-// //        var hexHash = spark.end();                      // hex hash
-//       }
-
-//     _write(chunk, enc, next) {
-//         this.spark.append(chunk);
-//         next();
-//     }
-// }
-
-
-
-// async function savePageEnex(accessToken: string, page: TreePage, pageContent: any, paths: { [key: string]: boolean }, writer: WritableStreamDefaultWriter, saveConfig: SaveConfig): Promise<any> {
-//     writer.write(`${`<note><title>${page.label}</title>`}<content><![CDATA[<?xml version="1.0" encoding="UTF-8" standalone="no"?>`);
-//     writer.write('<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">');
-//     if(saveConfig.images) {
-//         let images = pageContent.getElementsByTagName('img');
-
-//         for (const image of images) {
-//             let src = image.getAttribute('data-fullres-src') || image.getAttribute('src');
-//             if (!src) continue;
-//             let type = image.getAttribute('data-fullres-src-type') || image.getAttribute('data-src-type');
-
-//             const client = getAuthenticatedClient(accessToken);
-//             const id = src.split(/\//).slice(0, -1).pop();
-//             const extension = type.split(/\//).pop();
-//             const fileName = id + '.' + extension;
-
-
-//             var spark = new SparkMD5();
-//             let md5stream = new Writable();
-//             md5stream._write = function (chunk, encoding, done) {
-//               spark.append(chunk);
-//               done();
-//             };
-
-//             Buffer.
-
-//             client.api(src).getStream((error, resourceStream) => {
-//                 resourceStream.pipe(md5stream).pipe(base64stream).on('error', console.log);
-//             });
-
-//             md5stream.on('close', () => md5stream.end());
-//     // writer.write({
-//     //     name: path + '/' + relResourcePath,
-//     //     stream: () => resourceStream
-//     // });
-
-//     // return relResourcePath;
-
-
-//             // imgObjPromises.push(saveResource(accessToken, src, type, path, paths, writer).then((relResourcePath) => image.setAttribute('src', relResourcePath)));
-//             // imgObjPromises.push(saveResource(accessToken, src, type, '', paths, writer).then((relResourcePath) => image.setAttribute('src', relResourcePath)));
-//         }
-//     }
-
-
-
-//     writer.write('<en-note');
-//     for(const attribute of ['bgcolor', 'text', 'style', 'title', 'lang', 'xml:lang', 'dir']) {
-//         if(pageContent.body.hasAttribute(attribute)) {
-//             writer.write(` ${attribute}="${pageContent.body.getAttribute(attribute)}"`)
-//         }
-//     }  
-//     writer.write('>');
-
-//     pageContent.body
-//     if (saveConfig.html) {
-//         writer.write({
-//             name: '/html' + pagePath,
-//             stream: () => new Response(pageContent.documentElement.outerHTML).body
-//         });
-//     }
-// }
-
-
 
 async function saveResource(accessToken: string, src: string, type: string, path: string, paths: { [key: string]: boolean; }, writer: WritableStreamDefaultWriter): Promise<string> {
     const client = getAuthenticatedClient(accessToken);
